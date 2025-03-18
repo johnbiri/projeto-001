d9036a8e2339efae253655256b34a843a21ddb28
John Biri <johnhf12345@gmail.com>
2025-03-17 22:15:14 -0300

    up

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/css/main.css',
+        '/script.js',
+        '/assets/img/logos/icon-192x192.png',
+        '/assets/img/logos/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
