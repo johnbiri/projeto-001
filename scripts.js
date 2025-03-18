1f9a5d6bb0e2ece50e9c08b1b14700a4f5a7d8c5
John Biri <johnhf12345@gmail.com>
2025-03-17 22:04:42 -0300

    Up

--- scripts.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(() => console.log('Service Worker registrado!'))
-    .catch(err => console.error('Erro ao registrar o Service Worker:', err));
-}
